c88dcfc153481bfce5910c81262a46ded2ec7fb3
root <root@172.26.77.99>
2026-07-31 08:07:46 +0000

    fix: add DESI_HITS branch to exhaustive when expression
    The when(selectedSource) block in searchWithSource() must cover all
    enum values. Added DESI_HITS -> searchJioSaavn (Desi Hits catalog
    songs all come from JioSaavn, so search routes there too).

--- app/src/main/java/com/salmanlaghari/pulsemusicplayerai/presentation/youtube/YouTubeScreen.kt
+++ app/src/main/java/com/salmanlaghari/pulsemusicplayerai/presentation/youtube/YouTubeScreen.kt
@@ -119,6 +119,7 @@ fun YouTubeScreen(
         when (selectedSource) {
             MusicSource.ALL -> viewModel.searchAllSources(query)
             MusicSource.JIOSAAVN -> viewModel.searchJioSaavn(query)
+            MusicSource.DESI_HITS -> viewModel.searchJioSaavn(query)
             MusicSource.APPLE_MUSIC -> viewModel.searchAppleMusic(query)
             MusicSource.SPOTIFY -> viewModel.searchSpotify(query)
             MusicSource.YOUTUBE_MUSIC -> viewModel.searchYouTubeMusic(query)
